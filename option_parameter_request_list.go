89e2a6ea0624792aa0b77439b42d58ab367317e5
corbin <corbingrossen@gmail.com>
2025-03-01 21:15:42 -0500

    renamed option files and added option_paramter_request_list

---
+++ option_parameter_request_list.go
@@ -0,0 +1,41 @@
+package dhcp
+
+type OptionDataParameterRequestList struct {
+	List []OptionCode
+}
+
+func (optD OptionDataParameterRequestList) Raw() []byte {
+	data := make([]byte, len(optD.List))
+	for i, optC := range optD.List {
+		data[i] = byte(optC)
+	}
+
+	return data
+}
+
+func (optD OptionDataParameterRequestList) Add(optC OptionCode) {
+        optD.List = append(optD.List, optC)
+}
+
+func BuildOptionDataParameterRequestList(data []byte) (OptionData, error) {
+	list := make([]OptionCode, len(data))
+	for i, b := range data {
+		list[i] = OptionCode(b)
+	}
+	return OptionDataParameterRequestList{List: list}, nil
+}
+
+func NewOptionParameterRequestList(optCodes ...OptionCode) Option {
+        opt := Option{
+                Code: OptionCodeParameterRequestList,
+                Data: OptionDataParameterRequestList{
+                        List: make([]OptionCode, 0),
+                },
+        }
+
+        for _, optCode := range optCodes {
+                opt.Data.(OptionDataParameterRequestList).Add(optCode)
+        }
+
+        return opt
+}
--- optioncodeinfo.go
+++ optioncodeinfo.go
@@ -6,19 +6,20 @@ type OptionCodeInfo struct {
 }
 
 const (
-	OptionCodePad                 OptionCode = OptionCode(0)
-	OptionCodeSubnetMask          OptionCode = OptionCode(1)
-	OptionCodeRouter              OptionCode = OptionCode(3)
-	OptionCodeTimeServer          OptionCode = OptionCode(4)
-	OptionCodeNameServer          OptionCode = OptionCode(5)
-	OptionCodeDNS                 OptionCode = OptionCode(6)
-	OptionCodeLogServer           OptionCode = OptionCode(7)
-	OptionCodeQuoteOfTheDayServer OptionCode = OptionCode(8)
-	OptionCodeLRPServer           OptionCode = OptionCode(9)
-	OptionCodeHostname            OptionCode = OptionCode(12)
-	OptionCodeDomainname          OptionCode = OptionCode(15)
-	OptionCodeRootPath            OptionCode = OptionCode(17)
-	OptionCodeMessageType         OptionCode = OptionCode(53)
+	OptionCodePad                  OptionCode = OptionCode(0)
+	OptionCodeSubnetMask           OptionCode = OptionCode(1)
+	OptionCodeRouter               OptionCode = OptionCode(3)
+	OptionCodeTimeServer           OptionCode = OptionCode(4)
+	OptionCodeNameServer           OptionCode = OptionCode(5)
+	OptionCodeDNS                  OptionCode = OptionCode(6)
+	OptionCodeLogServer            OptionCode = OptionCode(7)
+	OptionCodeQuoteOfTheDayServer  OptionCode = OptionCode(8)
+	OptionCodeLRPServer            OptionCode = OptionCode(9)
+	OptionCodeHostname             OptionCode = OptionCode(12)
+	OptionCodeDomainname           OptionCode = OptionCode(15)
+	OptionCodeRootPath             OptionCode = OptionCode(17)
+	OptionCodeMessageType          OptionCode = OptionCode(53)
+	OptionCodeParameterRequestList OptionCode = OptionCode(55)
 
 	OptionCodeEnd OptionCode = OptionCode(255)
 )
@@ -31,5 +32,9 @@ var OptionCodeToInfo map[OptionCode]OptionCodeInfo = map[OptionCode]OptionCodeIn
 	OptionCodeMessageType: OptionCodeInfo{
 		String:      "DHCP Message Type",
 		DataBuilder: BuildOptionDataMessageType,
+	},
+	OptionCodeParameterRequestList: OptionCodeInfo{
+		String:      "Parameter Request List",
+		DataBuilder: BuildOptionDataParameterRequestList,
 	},
 }
