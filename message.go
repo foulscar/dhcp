bcbad317ade8ca6e843c3f01d79a195d9dd51e7f
corbin <corbingrossen@gmail.com>
2025-03-02 09:21:00 -0500

    clean up message unmarshaling

--- message.go
+++ message.go
@@ -137,31 +137,23 @@ func MarshalMessage(data []byte) (*Message, error) {
 }
 
 func (msg *Message) Unmarshal() []byte {
-	var data []byte
+        data := make([]byte, 240)
 
-	data = append(data,
-		byte(msg.BOOTPMessageType),
-		byte(msg.HardwareAddrType),
-		byte(msg.HardwareAddrLen),
-		byte(msg.HopCount),
-	)
-	binary.BigEndian.AppendUint32(data, msg.TransactionID)
-	binary.BigEndian.AppendUint16(data, msg.SecsElapsed)
-	binary.BigEndian.AppendUint16(data, msg.Flags)
-	data = append(data, []byte(msg.ClientIPAddr)...)
-	data = append(data, []byte(msg.YourIPAddr)...)
-	data = append(data, []byte(msg.ServerIPAddr)...)
-	data = append(data, []byte(msg.GatewayIPAddr)...)
-        cHardwareAddr := [16]byte{}
-        copy(cHardwareAddr[:], []byte(msg.ClientHardwareAddr))
-        data = append(data, cHardwareAddr[:]...)
-	sHostname := [64]byte{}
-	copy(sHostname[:], msg.ServerHostname)
-	data = append(data, sHostname[:]...)
-	bootFilename := [128]byte{}
-	copy(bootFilename[:], msg.BootFilename)
-	data = append(data, bootFilename[:]...)
-	data = append(data, MagicCookie...)
+	data[0] = byte(msg.BOOTPMessageType)
+	data[1] = byte(msg.HardwareAddrType)
+	data[2] = byte(msg.HardwareAddrLen)
+	data[3] = byte(msg.HopCount)
+        binary.BigEndian.PutUint32(data[4:8], msg.TransactionID)
+        binary.BigEndian.PutUint16(data[8:10], msg.SecsElapsed)
+        binary.BigEndian.PutUint16(data[10:12], msg.Flags)
+        copy(data[12:16], []byte(msg.ClientIPAddr))
+        copy(data[16:20], []byte(msg.YourIPAddr))
+        copy(data[20:24], []byte(msg.ServerIPAddr))
+        copy(data[24:28], []byte(msg.GatewayIPAddr))
+        copy(data[28:44], []byte(msg.ClientHardwareAddr))
+        copy(data[44:108], msg.ServerHostname)
+        copy(data[108:236], msg.BootFilename)
+        copy(data[236:240], MagicCookie)
 	data = append(data, msg.Options.Unmarshal()...)
 
 	return data
