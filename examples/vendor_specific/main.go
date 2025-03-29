44b9369bd867b623a0258b9218a4674860bff5b5
corbin <corbingrossen@gmail.com>
2025-03-29 17:30:30 -0400

    fixed typo in vendor_specific example comments

--- examples/vendor_specific/main.go
+++ examples/vendor_specific/main.go
@@ -1,7 +1,7 @@
 // This example shows how to incorporate custom (vendor-specific) options, using the dhcp package.
 // This custom option will simply store a 'tag'.
 // This 'tag' is only valid when it is 'cat' or 'dog'.
-// This 'tag' is specific to this example and you could do whatever you want with the dataackage main
+// This 'tag' is specific to this example and you could do whatever you want with the data
 package main
 
 import (
